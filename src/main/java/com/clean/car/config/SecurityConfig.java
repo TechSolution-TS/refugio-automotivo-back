ed898db70cf037d106e289102e7cf1c5dfb8cbfe
Lucas Belém <lucas.belem@usto.re>
2025-10-12 11:05:56 -0300

    Ajustando cors

--- src/main/java/com/clean/car/config/SecurityConfig.java
+++ src/main/java/com/clean/car/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://techsolution-juridico.netlify.app/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://refugio-automotivo.netlify.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
